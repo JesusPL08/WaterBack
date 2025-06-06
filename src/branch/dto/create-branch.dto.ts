7c8593eb45255a60a6ee9e65792d8610068eaaf2
JesusPL08 <jesusnaruto01@hotmail.com>
2025-06-06 13:10:14 -0600

    add column in branch and modification variables with optional

--- src/branch/dto/create-branch.dto.ts
+++ src/branch/dto/create-branch.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsNumber, IsPositive } from 'class-validator';
+import { IsString, IsNumber, IsPositive,IsOptional } from 'class-validator';
 
 export class CreateBranchDto {
   @IsString()
@@ -8,28 +8,38 @@ export class CreateBranchDto {
   address: string;
 
   @IsString()
+  @IsOptional()
+  coordinate: string;
+
+  @IsString()
   phoneNumber: string;
 
   @IsString()
   manager: string;
 
   @IsNumber()
+  @IsOptional()
   @IsPositive()
   salePrice: number;
 
   @IsString()
+  @IsOptional()
   razonSocial: string;
 
   @IsString()
+  @IsOptional()
   rfc: string;
 
   @IsString()
+  @IsOptional()
   regimenFiscal: string;
 
   @IsString()
+  @IsOptional()
   codigoPostal: string;
 
   @IsString()
+  @IsOptional()
   usoCfdi: string;
 
   @IsNumber()
